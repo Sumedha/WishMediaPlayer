6e01dc4a4c1ef328d434b5179254da75c2438c80
Sumedha Dharmasiri <Sumedha.Dharmasiri@gmail.com>
2017-05-07 20:47:28 +0200

    Player Exit Button!

--- app/src/main/java/com/example/android/wishmediaplayer/MainActivity.java
+++ app/src/main/java/com/example/android/wishmediaplayer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.wishmediaplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnplayerexit = (ImageButton) findViewById(R.id.playerexit);
+        btnplayerexit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                finish();
+                System.exit(0);
+            }
+        });
+
     }
 }
